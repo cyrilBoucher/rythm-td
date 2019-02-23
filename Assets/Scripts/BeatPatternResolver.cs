3b4c0a8b1ec8011d92e01c3c83deb9efb50613a2
Cyril <cyril.boucher94260@gmail.com>
2019-02-23 20:00:32 +0100

    Add beat pattern validation
    [BeatPatternResolver]
    - Add beat pattern validation beahviour
    
    [DefenseSpawner]
    - Add test to validate a simple pattern

--- Assets/Scripts/BeatPatternResolver.cs
+++ Assets/Scripts/BeatPatternResolver.cs
@@ -1,18 +1,127 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class BeatPatternResolver
 {
     private BeatPattern _beatPattern;
+    private int beatIdToValidate;
+    private int currentPatternIndex = 0;
+    public float validationOffset = 0.2f;
 
-    public void setPattern(BeatPattern pattern)
+    public void SetPattern(BeatPattern pattern)
     {
         _beatPattern = pattern;
     }
 
-    public void run()
+    // This function can be called at any time during execution
+    // to start validating the current beat pattern. It is the responsibility
+    // of the caller to call it enough times to validate the whole patter.
+    // This function will return either true or false whether the pattern
+    // was validated or not.
+    public bool Run()
     {
-        
+        float timeToClosestBeatSec = BeatEngine.TimeToClosestBeatSec();
+
+        if (currentPatternIndex == 0)
+        {
+            if (timeToClosestBeatSec > validationOffset)
+            {
+                beatIdToValidate = BeatEngine.BeatId();
+            }
+            else
+            {
+                beatIdToValidate = BeatEngine.ClosestBeatId();
+            }
+        }
+
+        // Success
+        if (Input.GetMouseButtonDown(0) &&
+            Math.Abs(timeToClosestBeatSec) <= validationOffset &&
+            _beatPattern.At(currentPatternIndex) == BeatPattern.Input.OnBeat &&
+            beatIdToValidate == BeatEngine.ClosestBeatId())
+        {
+            Debug.Log(String.Format("Success on beat at frame {0}", Time.frameCount));
+
+            return Success();
+        }
+
+        // Success
+        if (!Input.GetMouseButtonDown(0) &&
+            timeToClosestBeatSec > validationOffset &&
+            _beatPattern.At(currentPatternIndex) == BeatPattern.Input.SkipBeat &&
+            beatIdToValidate == BeatEngine.ClosestBeatId())
+        {
+            Debug.Log("Success skip beat");
+
+            return Success();
+        }
+
+        // Fail
+        if (Input.GetMouseButtonDown(0) &&
+            Math.Abs(timeToClosestBeatSec) > validationOffset)
+        {
+            Debug.Log(String.Format("Failure: pressed outside zone at frame {0}", Time.frameCount));
+
+            return Failure();
+        }
+
+        // Fail
+        if (currentPatternIndex != 0 &&
+            !Input.GetMouseButtonDown(0) &&
+            timeToClosestBeatSec > validationOffset &&
+            _beatPattern.At(currentPatternIndex) == BeatPattern.Input.OnBeat &&
+            beatIdToValidate == BeatEngine.ClosestBeatId())
+        {
+            Debug.Log("Failure: no press when it should have been pressed");
+
+            return Failure();
+        }
+
+        // Fail
+        if (Input.GetMouseButtonDown(0) &&
+            Math.Abs(timeToClosestBeatSec) <= validationOffset &&
+            _beatPattern.At(currentPatternIndex) == BeatPattern.Input.SkipBeat)
+        {
+            Debug.Log("Failure: pressed, should have skipped");
+
+            return Failure();
+        }
+
+        // Fail
+        if (Input.GetMouseButtonDown(0) &&
+            Math.Abs(timeToClosestBeatSec) <= validationOffset &&
+            _beatPattern.At(currentPatternIndex) == BeatPattern.Input.OnBeat &&
+            beatIdToValidate != BeatEngine.ClosestBeatId())
+        {
+            Debug.Log("Failure: pressed on wrong beat");
+
+            return Failure();
+        }
+
+        return false;
+    }
+
+    bool Success()
+    {
+        currentPatternIndex++;
+
+        if (currentPatternIndex == _beatPattern.pattern.Count)
+        {
+            Debug.Log("Success");
+            currentPatternIndex = 0;
+
+            return true;
+        }
+
+        beatIdToValidate++;
+
+        return false;
+    }
+
+    bool Failure()
+    {
+        currentPatternIndex = 0;
+
+        return false;
     }
 }
--- Assets/Scripts/DefenseSpawner.cs
+++ Assets/Scripts/DefenseSpawner.cs
@@ -6,55 +6,23 @@ using UnityEngine;
 public class DefenseSpawner : MonoBehaviour {
 
     private BeatPattern _beatPattern = new BeatPattern();
-    private int _patternIndex;
-    private int _lastValidatedBeatId;
+    private BeatPatternResolver _beatPatternResolver = new BeatPatternResolver();
 
-	// Use this for initialization
-	void Start () {
-        _patternIndex = 0;
-        _lastValidatedBeatId = -1;
+    // Use this for initialization
+    void Start () {
         _beatPattern.Add(BeatPattern.Input.OnBeat);
-        _beatPattern.Add(BeatPattern.Input.SkipBeat);
         _beatPattern.Add(BeatPattern.Input.OnBeat);
+
+        _beatPatternResolver.SetPattern(_beatPattern);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        BeatPattern.Input expectedInput = BeatPattern.Input.SkipBeat;
-        if (Input.GetMouseButtonDown(0))
+        bool result = _beatPatternResolver.Run();
+
+        if (result)
         {
-            expectedInput = BeatPattern.Input.OnBeat;
+            Debug.Log("IT WOOOOOOOOOOOOORKS");
         }
-
-        float timeToClosestBeatSec = BeatEngine.TimeToClosestBeatSec();
-
-        if (Math.Abs(timeToClosestBeatSec) > 0.2f &&
-            expectedInput == BeatPattern.Input.SkipBeat)
-        {
-            return;
-        }
-
-        if (Math.Abs(timeToClosestBeatSec) <= 0.2f &&
-            _beatPattern.At(_patternIndex) == expectedInput &&
-            _lastValidatedBeatId != BeatEngine.ClosestBeatId())
-        {
-            Debug.Log(string.Format("Success on beat {0}!", BeatEngine.ClosestBeatId()));
-
-            _patternIndex++;
-            _lastValidatedBeatId = BeatEngine.ClosestBeatId();
-
-            return;
-        }
-
-        Debug.Log(string.Format("Failure on beat {0}!", BeatEngine.ClosestBeatId()));
-
-        _patternIndex = 0;
-        _lastValidatedBeatId = -1;
 	}
-
-    private void resetBeatPatternIndex()
-    {
-        Debug.Log("Resetting pattern");
-        _patternIndex = 0;
-    }
 }
