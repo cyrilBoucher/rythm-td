342908b408ba53636edaf9e655ff2656b59525a2
Cyril <cyril.boucher94260@gmail.com>
2019-02-26 17:41:34 +0100

    Clean EnemyController

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,72 +4,33 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-    private LinkedList<Vector3> positions = new LinkedList<Vector3>();
     private Vector3 nextPosition;
     private float smoothTime = 1.0f;
     private int currentBeatId = 0;
     private Vector3 velocity;
-    public int life;
     private BoxCollider2D _boundingBox;
-
-    bool isNextPositionReached()
-    {
-        return transform.position == nextPosition;
-    }
+    public int life;
+    private Vector3 _spawnPosition;
 
 	// Use this for initialization
-	void Start () {
-        _boundingBox = GetComponent<BoxCollider2D>();
-       /* Vector3 newPosition = transform.position + new Vector3(1.0f, 0.0f, 0.0f);
-        positions.AddLast(newPosition);
-        newPosition += new Vector3(1.0f, 0.0f, 0.0f);
-        positions.AddLast(newPosition);
-        newPosition += new Vector3(0.0f, 1.0f, 0.0f);
-        positions.AddLast(newPosition);
-
-        nextPosition = positions.First.Value;*/
+	void Start ()
+    {
+        _spawnPosition = transform.position;
     }
 
 	// Update is called once per frame
-	void Update () {
-        /* if (positions.Count == 0)
-         {
-             return;
-         }
-
-         int beatEngineId = BeatEngine.BeatId();
-         if (beatEngineId != currentBeatId)
-         {
-             positions.RemoveFirst();
-
-             if (positions.Count == 0)
-             {
-                 return;
-             }
-
-             nextPosition = positions.First.Value;
-             smoothTime = BeatEngine.RemainingTimeUntilNextBeatSec() / 2.0f;
-             print(string.Format("smooth time is {0}", smoothTime));
-             currentBeatId = beatEngineId;
-
-             print("changing position for " + nextPosition);
-         }
-
-         GetComponent<Transform>().position = Vector3.SmoothDamp(GetComponent<Transform>().position, nextPosition, ref velocity, smoothTime);*/
-
+	void Update ()
+    {
         int beatEngineId = BeatEngine.BeatId();
         if (beatEngineId != currentBeatId)
         {
             Vector3 direction = new Vector3(1.0f, 0.0f, 0.0f);
-            nextPosition = GetComponent<Transform>().position + direction;
+            nextPosition = transform.position + direction;
             smoothTime = BeatEngine.RemainingTimeUntilNextBeatSec() / 2.0f;
-            //print(string.Format("smooth time is {0}", smoothTime));
             currentBeatId = beatEngineId;
-
-            //print("changing position for " + nextPosition);
         }
 
-        GetComponent<Transform>().position = Vector3.SmoothDamp(GetComponent<Transform>().position, nextPosition, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, nextPosition, ref velocity, smoothTime);
     }
 
     public void TakeDamage(int damage)
