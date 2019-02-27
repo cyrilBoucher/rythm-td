8f22a765e0feedf7b9a52f9a2b403b9a5436c2b7
Cyril <cyril.boucher94260@gmail.com>
2019-02-27 16:04:03 +0100

    Add BeatCounter
    Beat counter is a simple object that attempts to show the beat in a more
    graphical way. Here it is a sprite that blinks along the beat

---
+++ Assets/Scripts/BeatCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatCounter : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color spriteColor = _spriteRenderer.color;
+        spriteColor.a = BeatEngine.RemainingTimeUntilNextBeatSec() / BeatEngine.SecondsPerBeat();
+
+        _spriteRenderer.color = spriteColor;
+    }
+}
