269dae9c5686d8df5c5bbfdeb39f2b28ec5fee8e
Cyril <cyril.boucher94260@gmail.com>
2019-03-04 18:40:39 +0100

    Modify map1 to be a yaml file
    - The yaml file now contains base position, enemy spawn position and enemy route
    - Add YamlDotNet plugin to project for yaml parsing
    - Add reference to YamlDotNet assembly file in MyScriptsAssembly
    
    [Map]
    - Modify loading to use yaml parser

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,10 +7,39 @@ using System;
 public class Map
 {
     public static EnemyRoute enemyRoute;
+    public static Vector3 enemySpawnPosition;
+    public static Vector3 basePosition;
 
     public static bool LoadMap()
     {
-        return LoadEnemyRoute();
+        enemyRoute = new EnemyRoute();
+
+        List<Vector3> decodedEnemyRoute = new List<Vector3>();
+
+        YamlDotNet.Serialization.Deserializer deserializer = new YamlDotNet.Serialization.DeserializerBuilder().Build();
+
+        TextAsset textAsset = Resources.Load<TextAsset>("map1");
+
+        Dictionary<String, List<int[]>> parsed = deserializer.Deserialize<Dictionary<String, List<int[]>>>(textAsset.text);
+
+        List<int[]> decodedBasePosition = parsed["base"];
+        basePosition = new Vector3(decodedBasePosition[0][0], decodedBasePosition[0][1], decodedBasePosition[0][2]);
+
+        List<int[]> decodedEnemySpawnPosition = parsed["enemySpawn"];
+        enemySpawnPosition = new Vector3(decodedEnemySpawnPosition[0][0], decodedEnemySpawnPosition[0][1], decodedEnemySpawnPosition[0][2]);
+
+        List<int[]> deserializedEnemyRoute = parsed["enemyRoute"];
+
+        foreach(int[] deserializedPosition in deserializedEnemyRoute)
+        {
+            decodedEnemyRoute.Add(new Vector3(deserializedPosition[0], deserializedPosition[1], deserializedPosition[2]));
+        }
+
+        enemyRoute.Set(decodedEnemyRoute);
+
+        return true;
+
+        //return LoadEnemyRoute();
     }
 
     private static bool LoadEnemyRoute()
