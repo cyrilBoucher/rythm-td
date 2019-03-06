666cc7fe0eb65e944777df086ef3017d4dd3dfbb
Cyril <cyril.boucher94260@gmail.com>
2019-03-06 21:42:34 +0100

    Refactor Map loading
    Broke loading into functions

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -9,64 +9,99 @@ public class Map
     public static EnemyRoute enemyRoute;
     public static Vector3 enemySpawnPosition;
     public static Vector3 basePosition;
+    private const string WrongMapFormatErrorMessage = "Wrong format used in map1.yaml";
 
     public static bool LoadMap()
     {
-        enemyRoute = new EnemyRoute();
-
-        List<Vector3> decodedEnemyRoute = new List<Vector3>();
-
         YamlDotNet.Serialization.Deserializer deserializer = new YamlDotNet.Serialization.DeserializerBuilder().Build();
 
         TextAsset textAsset = Resources.Load<TextAsset>("map1");
 
         Dictionary<String, List<int[]>> parsed = deserializer.Deserialize<Dictionary<String, List<int[]>>>(textAsset.text);
 
-        List<int[]> decodedBasePosition = parsed["base"];
-        basePosition = new Vector3(decodedBasePosition[0][0], decodedBasePosition[0][1], decodedBasePosition[0][2]);
-
-        List<int[]> decodedEnemySpawnPosition = parsed["enemySpawn"];
-        enemySpawnPosition = new Vector3(decodedEnemySpawnPosition[0][0], decodedEnemySpawnPosition[0][1], decodedEnemySpawnPosition[0][2]);
-
-        List<int[]> deserializedEnemyRoute = parsed["enemyRoute"];
-
-        foreach(int[] deserializedPosition in deserializedEnemyRoute)
+        if (!DeserializeBasePosition(parsed))
         {
-            decodedEnemyRoute.Add(new Vector3(deserializedPosition[0], deserializedPosition[1], deserializedPosition[2]));
+            return false;
         }
 
-        enemyRoute.Set(decodedEnemyRoute);
+        if (!DeserializeSpawnPosition(parsed))
+        {
+            return false;
+        }
 
-        return true;
-
-        //return LoadEnemyRoute();
+        return DeserializeEnemyRoute(parsed);
     }
 
-    private static bool LoadEnemyRoute()
+    static private bool DeserializeBasePosition(Dictionary<String, List<int[]>> deserializedMapData)
+    {
+        List<int[]> deserializedBasePosition = deserializedMapData["base"];
+
+        if (deserializedBasePosition.Count != 1)
+        {
+            Debug.LogError(WrongMapFormatErrorMessage);
+
+            return false;
+        }
+
+        int[] deserializedBasePositionVector3 = deserializedBasePosition[0];
+
+        if (deserializedBasePositionVector3.Length != 3)
+        {
+            Debug.LogError(WrongMapFormatErrorMessage);
+
+            return false;
+        }
+
+        basePosition = new Vector3(deserializedBasePositionVector3[0], deserializedBasePositionVector3[1], deserializedBasePositionVector3[2]);
+
+        return true;
+    }
+
+    static private bool DeserializeSpawnPosition(Dictionary<String, List<int[]>> deserializedMapData)
+    {
+        List<int[]> deserializedSpawnPosition = deserializedMapData["enemySpawn"];
+
+        if (deserializedSpawnPosition.Count != 1)
+        {
+            Debug.LogError(WrongMapFormatErrorMessage);
+
+            return false;
+        }
+
+        int[] deserializedSpawnPositionVector3 = deserializedSpawnPosition[0];
+
+        if (deserializedSpawnPositionVector3.Length != 3)
+        {
+            Debug.LogError(WrongMapFormatErrorMessage);
+
+            return false;
+        }
+
+        enemySpawnPosition = new Vector3(deserializedSpawnPositionVector3[0], deserializedSpawnPositionVector3[1], deserializedSpawnPositionVector3[2]);
+
+        return true;
+    }
+
+    static private bool DeserializeEnemyRoute(Dictionary<String, List<int[]>> deserializedMapData)
     {
         enemyRoute = new EnemyRoute();
 
-        List<Vector3> decodedEnemyRoute = new List<Vector3>();
+        List<int[]> deserializedEnemyRoute = deserializedMapData["enemyRoute"];
 
-        TextAsset textAsset = Resources.Load<TextAsset>("map1");
-
-        string[] vectors = textAsset.text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-
-        foreach(string vector in vectors)
+        List<Vector3> enemyRoutePositions = new List<Vector3>();
+        foreach (int[] deserializedEnemyRoutePositionVector3 in deserializedEnemyRoute)
         {
-            string[] vectorElements = vector.Split(' ');
-
-            if (vectorElements.Length != 3)
+            if (deserializedEnemyRoutePositionVector3.Length != 3)
             {
-                Debug.LogError("Wrong format used in map1.txt");
+                Debug.LogError(WrongMapFormatErrorMessage);
 
                 return false;
             }
 
-            decodedEnemyRoute.Add(new Vector3(float.Parse(vectorElements[0]), float.Parse(vectorElements[1]), float.Parse(vectorElements[2])));
+            enemyRoutePositions.Add(new Vector3(deserializedEnemyRoutePositionVector3[0], deserializedEnemyRoutePositionVector3[1], deserializedEnemyRoutePositionVector3[2]));
         }
 
-        enemyRoute.Set(decodedEnemyRoute);
+        enemyRoute.Set(enemyRoutePositions);
 
         return true;
     }
