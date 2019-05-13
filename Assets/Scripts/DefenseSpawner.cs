55d1c0f32d216f4dfde00a3b26d9f90bebcfb245
Cyril <cyril.boucher94260@gmail.com>
2019-05-13 16:51:26 +0200

    Finalise DefenseSpawner script
    - Create prefab for input feedback text used by DefenseSpawner
    
    [DefenseSpawner]
    - Add logic to kill instance of DefenseSpawner and replace it with an instance of BasicDefense
    - Add logic to pay resources when validating pattern
    - Add logic to display text for input feedback to show whether input was late, early, etc...
    - Fix bug where input was wrongly detected on any defense spawner rather current one

--- Assets/Scripts/DefenseSpawner.cs
+++ Assets/Scripts/DefenseSpawner.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenseSpawner : MonoBehaviour {
 
+    private Text _inputFeedbackText;
+    private GameObject _inputFeedbackTextGameObjectInstance;
     private BeatPattern _beatPattern = new BeatPattern();
     private BeatPatternResolver _beatPatternResolver = new BeatPatternResolver();
+
     public ResourcesController resourcesController;
-    public GameObject defenseGameObject;
+    public GameObject worldSpaceCanvasGameObject;
+    public GameObject inputFeedbackTextPrefab;
+    public GameObject defensePrefab;
 
     // Use this for initialization
     void Start () {
@@ -16,29 +22,61 @@ public class DefenseSpawner : MonoBehaviour {
         _beatPattern.Add(BeatPattern.Input.OnBeat);
 
         _beatPatternResolver.SetPattern(_beatPattern);
+
+        _inputFeedbackTextGameObjectInstance = Instantiate(inputFeedbackTextPrefab, worldSpaceCanvasGameObject.transform);
+        _inputFeedbackText = _inputFeedbackTextGameObjectInstance.GetComponent<Text>();
+
+        Color textColor = _inputFeedbackText.color;
+        textColor.a = 0.0f;
+        _inputFeedbackText.color = textColor;
+
+        _inputFeedbackTextGameObjectInstance.transform.position = transform.position + new Vector3(0.0f, 0.5f, 0.0f);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 
         bool input = CheckForInput();
 
         BeatPatternResolver.ReturnType result = _beatPatternResolver.Run2(input);
 
-        if (result != BeatPatternResolver.ReturnType.Waiting)
-        {
-            Debug.Log(_beatPatternResolver.EnumToString(result));
-        }
-
         if (result == BeatPatternResolver.ReturnType.Validated)
         {
-            DefenseController defenseController = defenseGameObject.GetComponent<DefenseController>();
+            DefenseController defenseController = defensePrefab.GetComponent<DefenseController>();
 
-            if (resourcesController.resourcesNumber >= defenseController.price)
+            if (resourcesController.resourcesNumber < defenseController.price)
             {
-                Instantiate(defenseGameObject, transform.position, Quaternion.identity);
-                resourcesController.resourcesNumber -= defenseController.price;
+                return;
             }
+
+            Instantiate(defensePrefab, transform.position, Quaternion.identity);
+            resourcesController.resourcesNumber -= defenseController.price;
+
+            Destroy(this.gameObject);
+
+            // TODO: Avoid doing that
+            Destroy(_inputFeedbackText.gameObject);
+
+            return;
+        }
+
+        Color textColor = _inputFeedbackText.color;
+        if (result != BeatPatternResolver.ReturnType.Waiting)
+        {
+            textColor.a = 1.0f;
+
+            _inputFeedbackText.color = textColor;
+            _inputFeedbackText.text = _beatPatternResolver.EnumToString(result);
+            Debug.Log(_beatPatternResolver.EnumToString(result));
+
+            return;
+        }
+
+        if (textColor.a > 0.0f)
+        {
+            textColor.a -= Math.Max(0.5f * Time.deltaTime, 0.0f);
+            _inputFeedbackText.color = textColor;
         }
     }
 
@@ -52,7 +90,7 @@ public class DefenseSpawner : MonoBehaviour {
 
             if (hit.collider != null)
             {
-                if (hit.transform.name == "DefenseSpawner")
+                if (hit.transform.GetComponent<BoxCollider2D>() == GetComponent<BoxCollider2D>())
                 {
                     return true;
                 }
