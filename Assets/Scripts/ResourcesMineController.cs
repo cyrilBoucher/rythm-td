a1c8948f32a055e163054b94fed1dbce0abc16ab
Cyril <cyril.boucher94260@gmail.com>
2019-05-25 18:13:35 +0200

    Add first draft of resources mine
    Resources mine will be mined by player executing a pattern on it, getting
    resources if they succeed

---
+++ Assets/Scripts/ResourcesMineController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class ResourcesMineController : MonoBehaviour
+{
+    public ResourcesController resourcesController;    
+    public GameObject inputFeedbackTextPrefab;
+    public GameObject worldSpaceCanvasGameObject;
+
+    private BeatPatternResolver _beatPatternResolver;
+    private InputFeedbackTextController _inputFeedbackTextController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _beatPatternResolver = new BeatPatternResolver();
+
+        BeatPattern beatPattern = new BeatPattern();
+        beatPattern.Add(BeatPattern.Input.Tap);
+
+        _beatPatternResolver.SetPattern(beatPattern);
+
+        GameObject inputFeedbackTextGameObjectInstance = Instantiate(inputFeedbackTextPrefab,
+            transform.position + new Vector3(0.0f, 0.5f, 0.0f),
+            Quaternion.identity,
+            worldSpaceCanvasGameObject.transform);
+
+        _inputFeedbackTextController = inputFeedbackTextGameObjectInstance.GetComponent<InputFeedbackTextController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BeatPattern.Input input = InputDetector.CheckForInput(GetComponent<BoxCollider2D>());
+
+        BeatPatternResolver.ReturnType result = _beatPatternResolver.Run2(input);
+
+        if (result == BeatPatternResolver.ReturnType.Validated)
+        {
+            resourcesController.resourcesNumber += 1;
+
+            _inputFeedbackTextController.ShowFeedback(BeatPatternResolver.EnumToString(BeatPatternResolver.ReturnType.Good));
+
+            return;
+        }
+
+        if (result != BeatPatternResolver.ReturnType.Waiting)
+        {
+            _inputFeedbackTextController.ShowFeedback(BeatPatternResolver.EnumToString(result));
+        }
+    }
+}
