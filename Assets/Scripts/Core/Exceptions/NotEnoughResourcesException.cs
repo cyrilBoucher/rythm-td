ae048517198bcf5a024b80bd4656981511e8d58b
Cyril <cyril.boucher94260@gmail.com>
2019-07-13 17:57:37 +0200

    Add exception when player does not have enough resources
    - Add exception folder containing NotEnoughResourcesException
    - Throw NotEnoughResourcesException when TakeResources is called
    with a resources number higher than the ResourcesController's one

---
+++ Assets/Scripts/Core/Exceptions/NotEnoughResourcesException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[Serializable()]
+public class NotEnoughResourcesException : Exception
+{
+    public NotEnoughResourcesException() : base() { }
+    public NotEnoughResourcesException(string message) : base(message) { }
+    public NotEnoughResourcesException(string message, Exception inner) : base(message, inner) { }
+
+    protected NotEnoughResourcesException(System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
--- Assets/Scripts/Core/ResourcesController.cs
+++ Assets/Scripts/Core/ResourcesController.cs
@@ -54,6 +54,11 @@ public class ResourcesController
             throw new ArgumentException("Resources number cannot be negative");
         }
 
+        if (resourcesNumber > _resourcesNumber)
+        {
+            throw new NotEnoughResourcesException(string.Format("Cannot take {0} as there is only {1} remaining", resourcesNumber, _resourcesNumber));
+        }
+
         _resourcesNumber -= resourcesNumber;
     }
 }
