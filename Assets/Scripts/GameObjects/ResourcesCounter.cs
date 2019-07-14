4f51681b639d53e1ed4ef8f3337be373ac246967
Cyril <cyril.boucher94260@gmail.com>
2019-07-14 15:20:40 +0200

    Use ResourcesController event to display resources
    - Add a new prefab called resources counter along with its script
    subscribing to the resourcesNumberChanged event in order to display
    - Replace previous instances of ResourcesIcon by ResourcesCounter

---
+++ Assets/Scripts/GameObjects/ResourcesCounter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourcesCounter : MonoBehaviour
+{
+    public Text resourcesText;
+
+    void OnEnable()
+    {
+        ResourcesController.ResourcesNumberChanged += OnResourcesNumberChanged;
+    }
+
+    void OnDisable()
+    {
+        ResourcesController.ResourcesNumberChanged -= OnResourcesNumberChanged;
+    }
+
+    void Start()
+    {
+        SetResourcesNumber(ResourcesController.GetResourcesNumber());
+    }
+
+    void OnResourcesNumberChanged()
+    {
+        SetResourcesNumber(ResourcesController.GetResourcesNumber());
+    }
+
+    void SetResourcesNumber(int resourcesNumber)
+    {
+        resourcesText.text = ResourcesController.GetResourcesNumber().ToString();
+    }
+}
